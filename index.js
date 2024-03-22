78eb70874338448545dbfeb50c40f3ccba30f311
Fabio Niglio <fabio_niglio@hotmail.com>
2024-03-23 00:44:24 +0100

    Player and initial canvas

---
+++ index.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 64 * 16;
+canvas.height = 64 * 9;
+
+const player = new Player();
+
+// let bottom = y + 100;
+function animated() {
+  window.requestAnimationFrame(animated);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.draw();
+  player.update();
+}
+
+animated();
